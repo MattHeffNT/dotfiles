2b541b22f55ebcdc5f4db8197ac0ff0e473daf1d
MattHeffNT <heffa2004@gmail.com>
2025-07-17 16:54:12 +1000

    remove lunar
    remove lunar and add neovim and plugins

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 install_zsh() {
 
   # zsh install
-  if ! command -v zsh &> /dev/null; then
+  if ! command -v zsh &>/dev/null; then
 
-	  echo "zsh not found. Installing..." 
+    echo "zsh not found. Installing..."
 
-	  #install zsh 
-	  sudo apt-get update && sudo apt install zsh 
-	  
-	  #Make zsh the default shell 
-	  chsh -s $(which zsh)
+    #install zsh
+    sudo apt-get update && sudo apt install zsh
 
-    	  echo "zsh installation complete"
+    #Make zsh the default shell
+    chsh -s $(which zsh)
+
+    echo "zsh installation complete"
 
   else
 
@@ -26,35 +26,34 @@ install_zsh() {
 install_homebrew_and_packages() {
 
   # Check if Homebrew is installed
-  
-  if ! command -v brew &> /dev/null; then
+
+  if ! command -v brew &>/dev/null; then
     echo "Homebrew not found. Installing..."
 
     # Install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     # Add Homebrew to PATH in ~/.bashrc or ~/.zshrc
-      echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
-      source ~/.zshrc
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.zshrc
+    source ~/.zshrc
 
     echo "Homebrew installed successfully."
 
     brew install chezmoi
-
 
   else
 
     echo "Homebrew is already installed."
 
     # check if chezmoi installed and if not install it
-    if ! command -v chezmoi &> /dev/null; then
-	    brew install chezmoi
+    if ! command -v chezmoi &>/dev/null; then
+      brew install chezmoi
 
-    echo "Homebrew packages installation complete."
+      echo "Homebrew packages installation complete."
 
-   else
-	echo "chezmoi already installed"
-   fi
+    else
+      echo "chezmoi already installed"
+    fi
   fi
 }
 
@@ -63,16 +62,16 @@ install_homebrew_and_packages() {
 apply_chezmoi() {
 
   # Check if chezmoi is installed
-  if command -v chezmoi &> /dev/null; then
+  if command -v chezmoi &>/dev/null; then
 
-  # Apply chezmoi configuration
+    # Apply chezmoi configuration
     echo "Applying chezmoi configuration..."
     chezmoi init
     chezmoi apply
 
     echo "chezmoi configuration applied successfully."
   else
-	  echo "hmm chezmoi issues"
+    echo "hmm chezmoi issues"
   fi
 }
 
