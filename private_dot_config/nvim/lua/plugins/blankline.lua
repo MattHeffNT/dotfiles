9f09618f7b9e2c18a495f27196380352fedd7a99
MattHeffNT <heffa2004@gmail.com>
2026-03-10 03:27:27 +1100

    add rainbow blanklines and use normal nvim colors

--- private_dot_config/nvim/lua/plugins/blankline.lua
+++ private_dot_config/nvim/lua/plugins/blankline.lua
@@ -1,41 +1,34 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  event = "LazyFile",
   main = "ibl",
-  event = "VeryLazy", -- Optional: lazy load
-  config = function()
-    --     local highlight = {
-    --     "RainbowRed",
-    --     "RainbowYellow",
-    --     "RainbowBlue",
-    --     "RainbowOrange",
-    --     "RainbowGreen",
-    --     "RainbowViolet",
-    --     "RainbowCyan",
-    -- }
-    local hooks = require("ibl.hooks")
+  opts = function()
+    -- Define the colors (hooks into TokyoNight colors)
+    local highlight = {
+      "RainbowRed",
+      "RainbowYellow",
+      "RainbowBlue",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+      "RainbowCyan",
+    }
 
-    -- -- Set highlight colors (auto-reset on colorscheme change)
-    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-      vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
-      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
-      vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
-      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
-      vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
-    end)
+    -- Create the actual highlight groups in Neovim
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 
-    require("ibl").setup({
+    return {
       indent = {
-        highlight = "highlight",
-        smart_indent_cap = true,
-        -- char = "|"
-
         char = "│",
+        highlight = highlight, -- Apply the rainbow list here
       },
-      scope = {
-        enabled = false,
-      },
-    })
+      scope = { enabled = false }, -- Rainbow usually looks better without a separate scope highlight
+    }
   end,
 }
